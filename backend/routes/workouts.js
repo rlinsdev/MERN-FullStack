a020067330fb8f89bb2628e1b1de6a3820f3079e
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-13 16:13:28 -0300

    Models and Schemas and Post method

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	res.json({msg: "Get all"});
+	// const workout = await Workout.get()
+	// res.status(200).json(workout);
 });
 
 router.get('/:id', (req, res) => {
 	res.json({msg: "Get Single"});
 });
 
-router.post('/', (req, res) => {
-	res.json({msg: "Post Single"});
+router.post('/', async (req, res) => {
+
+	const {title, load, reps} = req.body;
+
+	try {
+		const workout = await Workout.create({title, load, reps});
+		res.status(200).json(workout);
+	} catch (error) {
+		res.status(400).json({error: error.message});
+	}
 });
 
 router.delete('/:id', (req, res) => {
