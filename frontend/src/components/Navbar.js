b7a62ca7c94d5ef0a9d12732b19e31b5aa484045
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-16 12:19:23 -0300

    Home component - reading data from API

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
 		<header>
 			<div className="container">
 				<Link to="/">
-					<h1>Workout Body!!!!</h1>
+					<h1>Workout Body</h1>
 				</Link>
 
 			</div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
 const Home = () => {
+	const [workouts, setWorkouts] = useState(null)
+
+	useEffect(()=> {
+		const fetchWorkout = async () => {
+			const response = await fetch ('/api/workouts/')
+			const json = await response.json()
+
+			if (response.ok) {
+				setWorkouts(json)
+			}
+		}
+		fetchWorkout()
+	}, [])
+
 	return (
 		<div className="home">
-			<h2>Home!</h2>
+			<div className="workouts">
+				oi
+				{workouts && workouts.map((workout) => (
+					<p key={workout._id}>{workout.title}</p>
+				))}
+			</div>
 		</div>
 	)
 }
