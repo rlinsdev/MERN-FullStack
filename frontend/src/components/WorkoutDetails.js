4d5e6beed8679b737ea23a43df19611631b1ef05
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-17 16:02:17 -0300

    formating date - fns date

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
+//date fs
+import formatDistanceToNow  from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({ workout }) => {
 	const {dispatch} = useWorkoutsContext()
 
@@ -21,7 +24,7 @@ const WorkoutDetails = ({ workout }) => {
 			<h4>{workout.title}</h4>
 			<p><strong>load (Kg): </strong>{workout.load}</p>
 			<p><strong>Reps: </strong>{workout.reps}</p>
-			<p>{workout.createdAt}</p>
+			<p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix:true})}</p>
 			<span onClick={handleClick}
 			className="material-symbols-outlined">delete</span>
 		</div>
