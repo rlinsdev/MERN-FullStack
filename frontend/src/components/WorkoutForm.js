f00b0cac098886ab593b9f92185a1d15347e7c9c
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-17 13:06:18 -0300

    Updating form before add new reg

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,7 +1,9 @@
-const { useState } = require("react")
+import { useState } from  "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 const WorkoutForm = () => {
 
+	const { dispatch } = useWorkoutsContext();
 	const [title, setTitle] = useState('')
 	const [load, setLoad] = useState('')
 	const [reps, setReps] = useState('')
@@ -29,6 +31,7 @@ const WorkoutForm = () => {
 			setReps('')
 			setError(null)
 			console.log('new Workout added')
+			dispatch({type:'CREATE_WORKOUT', payload: json})
 		}
 	}
 
